c1f7f1cc2bfd0643409858c1b14633c8f82b013b
joao25dev <joaodev2025@gmail.com>
2025-03-26 19:06:32 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -8,27 +8,27 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
+		Scanner sc= new Scanner(System.in);
 		
-		double A, B, C, TRIANGULO, CIRCULO, TRAPEZIO, QUADRADO, RETANGULO;
-		final double pi = 3.14159;
 		
-		A = sc.nextDouble();
-		B = sc.nextDouble();
-		C = sc.nextDouble();
+			
+			int N = sc.nextInt();
+	    
+			if (N % 2 == 0) {
+			System.out.println("PAR");
+			}
+			else {
+			System.out.println("IMPAR");
+			}
+
+				
 		
-		TRIANGULO = 0.5 * A * C;
-		CIRCULO = pi * C * C;
-		TRAPEZIO = ((A + B) / 2) * C;
-		QUADRADO = B * B;
-		RETANGULO = A * B;
 		
-		System.out.printf("TRIANGULO: %.3f%n", TRIANGULO);
-		System.out.printf("CIRCULO: %.3f%n", CIRCULO);
-		System.out.printf("TRAPEZIO: %.3f%n", TRAPEZIO);
-		System.out.printf("QUADRADO: %.3f%n", QUADRADO);
-		System.out.printf("RETANGULO: %.3f%n", RETANGULO);
 		
+			
 		sc.close();
+	
+		
 	}
+
 }
